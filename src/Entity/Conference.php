ae94b9075a38b1c6517537c298bcfb7d2cc6e91e
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-02 15:26:39 +0300

    add conference entity listener

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,6 +6,7 @@ use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
@@ -135,5 +136,12 @@ class Conference
         $this->slug = $slug;
 
         return $this;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string)$slugger->slug((string)$this)->lower();
+        }
     }
 }
