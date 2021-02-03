a07abd572cd4f562fb13ddc624034593ca322f1b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-03 08:29:08 +0300

    add easy admin redirect

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -98,6 +98,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
     }
 
     protected function getLoginUrl()
